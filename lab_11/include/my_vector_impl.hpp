44bff02310a891a4b9b55c0eaea0ff3d3f46d637
Arseny Yusufov <senya.yusufov@yandex.ru>
2021-04-02 12:35:16 +0300

    fix

--- lab_11/include/my_vector_impl.hpp
+++ lab_11/include/my_vector_impl.hpp
@@ -4,8 +4,12 @@
 
 namespace containers {
   template <typename T>
-  void my_vector<T>::push_back(T& t) {
+  void my_vector<T>::push_back(const T& t) {
+    if (_size + 1 > _capacity)
+      reserve(std::max(_size << 1, (std::size_t)2));
 
+    new(_array + _size)T(t);
+    _size++;
   }
 
 }
